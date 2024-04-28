2df000bc1810543d7285f9f1087e6934d47116c8
Urban Eriksson <urban@validio.io>
2024-04-28 20:59:42 +0200

    sync

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-document.querySelectorAll('.toggle-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        this.style.backgroundColor = this.style.backgroundColor === 'red' ? 'grey' : 'red';
+// document.querySelectorAll('.toggle-btn').forEach(button => {
+//     button.addEventListener('click', function() {
+//         this.style.backgroundColor = this.style.backgroundColor === 'red' ? 'grey' : 'red';
+//     });
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Array of button ids
+    const buttonIds = ['button1', 'button2', 'button3','button4', 'button5', 'button6','button7', 'button8', 'button9'];
+
+    // Initialize or load button states
+    buttonIds.forEach(id => {
+        const storedState = localStorage.getItem(id);
+        const button = document.getElementById(id);
+        if (storedState === 'on') {
+            button.classList.add('button-on');
+        } else {
+            button.classList.add('button-off');
+        }
+
+        // Adding click event listener to toggle state
+        button.addEventListener('click', () => toggleButton(id));
     });
+
+    function toggleButton(buttonId) {
+        const button = document.getElementById(buttonId);
+        const isOn = button.classList.contains('button-on');
+
+        if (isOn) {
+            button.classList.remove('button-on');
+            button.classList.add('button-off');
+            localStorage.setItem(buttonId, 'off');
+        } else {
+            button.classList.remove('button-off');
+            button.classList.add('button-on');
+            localStorage.setItem(buttonId, 'on');
+        }
+    }
 });
